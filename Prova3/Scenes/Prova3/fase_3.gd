71de0a81f30e513b81d12d15e87a75ad0b54ad46
Herik Pereira de Sousa <herikp345@gmail.com>
2024-11-19 16:49:28 -0300

    ptobs

---
+++ Prova3/Scenes/Prova3/fase_3.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Player.global_position.x = 55
+	$Player.global_position.y = 280
+	$"/root/Global".fase = 3
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ Prova3/Scripts/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+var JUMP_VELOCITY = -400.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		$Animacao.scale.x = direction
+
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	setAnimation()
+	setCamera()
+	move_and_slide()
+
+func setAnimation():
+	var anim = "Idle"
+	if (velocity.x != 0 and is_on_floor()):
+		anim = "Run"
+	if (!is_on_floor()):
+		anim = "Jump"
+	$Animacao.animation = anim
+
+func setCamera():
+	if $"/root/Global".fase == 1:
+		$Camera1.limit_right = 3040
+	if $"/root/Global".fase == 2:
+		$Camera1.limit_right = 640
+	if $"/root/Global".fase == 3:
+		$Camera1.limit_right = 1500
+	
